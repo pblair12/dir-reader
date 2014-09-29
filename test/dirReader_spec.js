5f1e6531595bb8e5be9c878a3a1f518a37a3bae5
Patrick Blair <pblair12@gmail.com>
2014-09-29 04:51:31 -0400

    - changing the syntax of the callbacks - removing throwing an exception and instead returning an error object to the callbacks - added more tests

--- test/dirReader_spec.js
+++ test/dirReader_spec.js
@@ -4,12 +4,12 @@ var should = require('should');
 describe("Registration", function () {
   var dirItems = [];
 
-  before(function (done) {
-    var callback = function(data) {
+  beforeEach(function (done) {
+    var callback = function(error, data) {
       dirItems = data;
       done();
     };
-    dirReader.getDirItems({dirPath: './test/mock'}, callback);
+    dirReader.getDirItems({dirPath: './test/mock/'}, callback);
   });
 
   // happy path
@@ -17,18 +17,18 @@ describe("Registration", function () {
     it("is successful", function() {
       dirItems.length.should.equal(2);
       dirItems[0].fileName.should.equal('file1.html');
-      dirItems[0].dirPath.should.equal('./test/mock');
+      dirItems[0].dirPath.should.equal('./test/mock/');
       dirItems[1].fileName.should.equal('file2.txt');
-      dirItems[1].dirPath.should.equal('./test/mock');
+      dirItems[1].dirPath.should.equal('./test/mock/');
     });
   });
   describe("a valid read of a file contents", function () {
-    it("is successful", function() {
-      var callback = function(data) {
+    it("is successful", function(done) {
+      var callback = function(error, data) {
+        (error === null).should.be.true;
         dirItems[1] = data;
         dirItems[1].fileName.should.equal('file2.txt');
-        dirItems[1].dirPath.should.equal('./test/mock');
-        dirItems[1].fileName.should.equal('file2.txt');
+        dirItems[1].dirPath.should.equal('./test/mock/');
         dirItems[1].fileContent.should.equal('file2 contents');
         done();
       };
@@ -36,13 +36,35 @@ describe("Registration", function () {
     });
   });
 
-  describe("a bad dirctory path", function () {
-    it("is not successful", function() {
-      try {
-        dirReader.getDirItems({dirPath: './mock_does_not_exist'});
-      } catch (e) {
-        e.should.be.defined();
-      }
+  describe("an invalid getDirItems call", function () {
+    it("is not a valid directory path", function(done) {
+      var callback = function(error, data) {
+        error.should.be.defined;
+        error.message.should.startWith('ENOENT');
+        done();
+      };
+      dirReader.getDirItems({dirPath: './mock_does_not_exist'}, callback);
+    });
+  });
+
+  describe("an invalid getFileContents call", function () {
+    it("is not a valid directory path", function(done) {
+      var callback = function(error, data) {
+        error.should.be.defined;
+        error.message.should.startWith('ENOENT');
+        done();
+      };
+      dirItems[1].dirPath = './mock_does_not_exist/';
+      dirReader.getFileContents({dirItem: dirItems[1]}, callback);
+    });
+    it("is not a valid file name", function(done) {
+      var callback = function(error, data) {
+        error.should.be.defined;
+        error.message.should.startWith('ENOENT');
+        done();
+      };
+      dirItems[1].fileName = 'doesNotExist.txt';
+      dirReader.getFileContents({dirItem: dirItems[1]}, callback);
     });
   });
 });
